7c4e056c51772b80d24179d641e60aa04c664a8a
xiaokangwang <xiaokangwang@xiaokang-Server.(none)>
2013-01-19 22:12:02 +0800

    basic buld connection

--- nextreq.php
+++ nextreq.php
@@ -4,4 +4,17 @@ This is a script to make a line-based download info so that we will able to down
 */
 
 //TODO:(UP)
+
+//mysql connection infomation,change for your own server
+$mysqlserv="";
+$mysqlusr="";
+$mysqlpasswd="";
+$mysqldbname="";
+
+//connect to MYSQL server
+$con = mysql_connect($mysqlserv,$mysqlusr,$mysqlpasswd);
+//select database
+mysql_select_db($mysqldbname, $con);
+
+
 ?>
