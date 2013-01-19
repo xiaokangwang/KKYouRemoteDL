6eba580a00657966c3c03fb5fc9a138072418d7f
xiaokangwang <xiaokangwang@xiaokang-Server.(none)>
2013-01-19 20:36:20 +0800

    postrequest.php connect to db

--- postrequest.php
+++ postrequest.php
@@ -7,9 +7,16 @@ This is a script to record the things that user requested to get.
 $mysqlserv="";
 $mysqlusr="";
 $mysqlpasswd="";
+$mysqldbname="";
 
-
+//prepare infomation
 $useremail=$_POST["email"];
 $linktodl=$_POST["linktodl"];
+
+//connect to MYSQL server
 $con = mysql_connect($mysqlserv,$mysqlusr,$mysqlpasswd);
+//select database
+mysql_select_db($mysqldbname, $con);
+
+
 ?>
