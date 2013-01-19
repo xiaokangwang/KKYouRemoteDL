a2b6e0c136581cddff9fe2546175d203166ae20b
xiaokangwang <xiaokangwang@xiaokang-Server.(none)>
2013-01-19 21:54:29 +0800

    and again, some plan

---
+++ godownload.cpp
@@ -0,0 +1,5 @@
+/* 
+In this file we will let the progrom know about what to download and also perpare a e-mail to send so that 
+People who made request can know their video was downloaded successfully.
+*/
+//TODO:(UP)
---
+++ sendmail.php
@@ -0,0 +1,6 @@
+<?php
+/*
+In this script we will send a e-mail so that people who wants theirs video will know about it. 
+*/
+//TODO:(UP)
+?>
