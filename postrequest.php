db8bb4ddb6e88ec30df0a6da14b8990e8eba4e5f
xiaokangwang <xiaokangwang@xiaokang-Server.(none)>
2013-01-19 20:51:29 +0800

    postrequest was finished(just write to end of dealing,I means theres a lots of things to do still)

--- postrequest.php
+++ postrequest.php
@@ -18,5 +18,20 @@ $con = mysql_connect($mysqlserv,$mysqlusr,$mysqlpasswd);
 //select database
 mysql_select_db($mysqldbname, $con);
 
+//write request
+$sql="INSERT INTO RequestsR (useremail, linktodl)
+VALUES
+('"$useremail"','"$linktodl"')";
+
+//progress request
+if (!mysql_query($sql,$con))
+  {
+  	//Sorry for that but request was unssuccessful , so tell the client request won't be progressed.
+  	//TODO: tell client why
+  	die("FAIL_SQL_REQDIE");
+  }
+
+//Finally show the successful signal to the client
+echo "SUCC_REQADD";
 
 ?>
