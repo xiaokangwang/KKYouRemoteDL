4b22cbdd6d496ef6a762914e704cec127a392689
xiaokangwang <xiaokangwang@xiaokang-Server.(none)>
2013-02-07 21:35:53 +0800

    new way to solve is found

--- godownload.cpp
+++
@@ -1,5 +0,0 @@
-/* 
-In this file we will let the progrom know about what to download and also perpare a e-mail to send so that 
-People who made request can know their video was downloaded successfully.
-*/
-//TODO:(UP)
--- sendmail.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-In this script we will send a e-mail so that people who wants theirs video will know about it. 
-*/
-//TODO:(UP)
-?>
