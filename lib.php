2301304a1893138c49d273646f2ad0eb5413357f
Xiaokang Wang <xiaokangwang@outlook.com>
2013-02-08 11:43:02 +0800

    request check

--- lib.php
+++ lib.php
@@ -11,6 +11,10 @@ if(requestURL==""){
 	return -1;
 }
 
+//in the submited url should not include ' or "
+if(stripos(requestURL,'"')!=false||stripos(requestURL,"'")!=false){
+	return -1;
+}
 
 
 }
--- proceed.php
+++ proceed.php
@@ -1,2 +1,24 @@
 <?php
+/*
+In this script,we will call the youtube-dl to download the movie, 
+as well as we respond the url of the file we will provide user to download.
+*/
+
+require_once("lib.php");
+
+
+/*
+prepare request detail
+*/
+
+//get from post
+$requestedURL_base64=$_POST["urlb64"];
+
+//debase64
+$requestedURL_org=base64_decode($requestedURL_base64);
+
+//check it is a good one.
+
+
+
 ?>
